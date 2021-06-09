464ff06472cf6105ce5d55e0e769259cc1ee2f93
angelrash53 <angelrash52@gmail.com>
2021-06-09 07:29:17 -0700

    initial commit

---
+++ datatype.py
@@ -0,0 +1,18 @@
+Data Types:
+1. String (str): contains alphanumerical values. e.g "Rashida Abdullabhai 18"
+username= "Abdullabhai" #Declaring a variable of type string implicitly
+username=str("Abdullabhai") #Declaring a variable of type string explicitly
+
+2. Integer (int): contains whole numbers from typing import ValuesView
+from 0 to infinity
+age=18
+age=int("18")
+
+3. Float:Decimal Values
+price=23.57
+
+a=2
+b=3
+If b>a
+print("")
+b !==c
